f68d274e23865520304026fc903b94897530aad3
Marcin Chowaniec <marcinchowaniec21@gmail.com>
2025-11-11 19:53:23 +0100

    fix: build

--- .wrangler/tmp/dev-kfFaT2/pages/_image.astro.mjs
+++
@@ -1,3 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { a as page } from '../chunks/image-endpoint_CC6oObS-.mjs';
-export { r as renderers } from '../chunks/_@astro-renderers_B70jUmW-.mjs';
